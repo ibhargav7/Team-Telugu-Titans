604b1b36a4da9de03e6ce65d5dc4dcd24a489041
Bhargav Inturi <bhargavinturi792@gmail.com>
2022-03-23 00:19:31 +0530

    Add meeting class

---
+++ src/meeting.py
@@ -0,0 +1,13 @@
+class Meeting:
+    def __init__(self,meetingid,duration,starttime,organiser,attendee,coursename):
+        self.meetingid=meetingid
+        self.duration=duration
+        self.starttime=starttime
+        self.organiser=organiser
+        self.attendee=attendee
+        self.coursename=coursename
+
+    def endMeet():
+        pass
+    def generateQR():
+        pass    
