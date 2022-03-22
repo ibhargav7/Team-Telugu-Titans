9cea99cbe2799fba74131e69bce1d05c769b7d64
Ravikanthmattipally <99465539+Ravikanthmattipally@users.noreply.github.com>
2022-03-23 00:19:04 +0530

    Add files via upload

---
+++ src/cources.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+
+
+class Courses:
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
---
+++ src/teacher.py
@@ -0,0 +1,8 @@
+class Teacher:
+    def __init__(self,name,roll,password):
+        self.name=name
+        self.roll=roll
+        self.password=password
+
+    def login():
+        pass    
